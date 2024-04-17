4170dd12f24b8e5cc52b85b001311b701daa502e
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2024-04-17 15:49:57 +0800

    wip: add amcl demo launch file

---
+++ p3dx_navigation/launch/amcl_demo.launch.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from os.path import join
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+  this_package_path = get_package_share_directory("p3dx_navigation")
+
+  localization_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(join(this_package_path, "launch", "localization.launch.py")),
+    launch_arguments={
+      "map_name" : "warehouse_world",
+    }.items()
+  )
+
+  navigation_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(join(this_package_path, "launch", "navigation.launch.py")),
+  )
+
+  return LaunchDescription([
+    localization_launch,
+    navigation_launch,
+  ])
