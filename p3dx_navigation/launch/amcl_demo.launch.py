a4e4633419f11996af9595fd0bc0b65ddfc693ec
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-03-28 16:10:54 +0800

    fix: tf error while sending goals
    Set use sim time as true!

--- p3dx_navigation/launch/amcl_demo.launch.py
+++ p3dx_navigation/launch/amcl_demo.launch.py
@@ -1,43 +1,50 @@
 #!/usr/bin/python3
 
 from os.path import join
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import (
-    LaunchConfiguration,
-    EqualsSubstitution,
-    NotEqualsSubstitution,
-)
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import (
+    EqualsSubstitution,
+    LaunchConfiguration,
+    NotEqualsSubstitution,
+)
 
 
 def generate_launch_description():
     this_package_path = get_package_share_directory("p3dx_navigation")
-    robot_namespace = LaunchConfiguration("robot_namespace", default="")
 
     localization_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             join(this_package_path, "launch", "localization.launch.py")
         ),
-        launch_arguments={
-            "map_name": "warehouse_world",
-            "robot_namespace": robot_namespace,
-        }.items(),
     )
 
     navigation_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             join(this_package_path, "launch", "navigation.launch.py")
         ),
-        launch_arguments={
-            "robot_namespace": robot_namespace,
-        }.items(),
     )
 
     return LaunchDescription(
         [
-            DeclareLaunchArgument("robot_namespace", default_value=""),
+            DeclareLaunchArgument(
+                "map_name",
+                default_value="warehouse_world",
+                description="Use to select 2d map name",
+            ),
+            DeclareLaunchArgument(
+                "robot_namespace",
+                default_value="",
+                description="Add namespace for nodes related to this robot",
+            ),
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="true",
+                description="Use simulation (Gazebo) clock if true",
+            ),
             localization_launch,
             navigation_launch,
         ]
