d331317ea3c4e116719f78f3c43385d9637c6fef
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-07-30 16:35:57 +0800

    fix: behaviour server deserve remapping too
    Noted that the composable nodes are not working or correctly remapping
    at the moment.

--- p3dx_navigation/launch/navigation.launch.py
+++ p3dx_navigation/launch/navigation.launch.py
@@ -190,7 +190,7 @@ def generate_launch_description():
                 respawn_delay=2.0,
                 parameters=[configured_params],
                 arguments=["--ros-args", "--log-level", log_level],
-                remappings=remappings,
+                remappings=controller_remappings,
             ),
             # ),
             Node(
