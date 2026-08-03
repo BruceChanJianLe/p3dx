2565c76e1e6bc54eb8d7d70394c51d52bdba989e
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-08-03 22:53:33 +0800

    fix: multirobot tf from gazebo
    If encoders is being use as odometry source, /tf needs to be broadcast
    over, this fix addressed that.

--- p3dx_gazebo/launch/spawn_p3dx.launch.py
+++ p3dx_gazebo/launch/spawn_p3dx.launch.py
@@ -148,6 +148,13 @@ def generate_launch_description():
         executable="parameter_bridge",
         namespace=robot_namespace,
         arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            PathJoinSubstitution(
+                [
+                    robot_namespace,
+                    "tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+                ]
+            ),
             PathJoinSubstitution(
                 [
                     robot_namespace,
@@ -189,6 +196,12 @@ def generate_launch_description():
             ),
         ],
         remappings=[
+            (
+                PathJoinSubstitution(
+                    ["/", robot_namespace, robot_namespace, "tf"]
+                ),
+                "/tf",
+            ),
             (
                 PathJoinSubstitution(
                     ["/", robot_namespace, robot_namespace, "RosAria/cmd_vel"]
